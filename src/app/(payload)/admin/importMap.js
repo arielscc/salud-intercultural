9ca0b4f8f0cfb33bb1d8f4bbfd476567965de91d
Ariel Chura <ariel.chura.c@gmail.com>
2026-05-26 23:38:27 -0400

    feat: add VercelBlobClientUploadHandler to import map

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -2,6 +2,7 @@ import { LeadListIntro as LeadListIntro_f9dc1c7957603944f42088a066b0493b } from 
 import { LeadDetailActions as LeadDetailActions_c24c8c90c0ed37095e1ca13f42c9f84c } from '@/payload/admin/LeadDetailActions'
 import { AdminQuickLinks as AdminQuickLinks_d1d7975d3074181947c3f150e87b0643 } from '@/payload/admin/AdminQuickLinks'
 import { AdminDashboard as AdminDashboard_1311e1d64d5f5a09a4764a18b4306283 } from '@/payload/admin/AdminDashboard'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 /** @type import('payload').ImportMap */
@@ -10,5 +11,6 @@ export const importMap = {
   "@/payload/admin/LeadDetailActions#LeadDetailActions": LeadDetailActions_c24c8c90c0ed37095e1ca13f42c9f84c,
   "@/payload/admin/AdminQuickLinks#AdminQuickLinks": AdminQuickLinks_d1d7975d3074181947c3f150e87b0643,
   "@/payload/admin/AdminDashboard#AdminDashboard": AdminDashboard_1311e1d64d5f5a09a4764a18b4306283,
+  "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e,
   "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
